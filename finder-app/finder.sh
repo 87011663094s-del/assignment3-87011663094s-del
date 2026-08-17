2575b4550525c92d03418d4e34bdf78f7abf5b63
87011663094s-del <87011663094s@gmail.com>
2026-08-17 15:13:41 +0500

    Update finder.sh

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Проверяем количество аргументов
 if [ $# -ne 2 ]; then
     echo "Error: Two arguments are required: <directory> <search_string>"
     exit 1
@@ -8,12 +9,17 @@ fi
 filesdir=$1
 searchstr=$2
 
+# Проверяем, существует ли директория
 if [ ! -d "$filesdir" ]; then
     echo "Error: Directory $filesdir does not exist."
     exit 1
 fi
 
+# Считаем количество файлов
 X=$(find "$filesdir" -type f | wc -l)
+
+# Считаем количество найденных строк
 Y=$(grep -r "$searchstr" "$filesdir" | wc -l)
 
-echo "The number of matching lines are $Y and number of files are $X"
+# Выводим строго ожидаемую автотестом строку
+echo "The number of files are $X and the number of matching lines are $Y"
