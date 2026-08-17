8e86db3e222f2b30d80b420e20f9267a9f512dae
87011663094s-del <87011663094s@gmail.com>
2026-08-17 16:05:23 +0500

    Create threading.h

---
+++ examples/autotest-validate/threading.h
@@ -0,0 +1,16 @@
+#ifndef EXAMPLES_THREADING_THREADING_H_
+#define EXAMPLES_THREADING_THREADING_H_
+
+#include <stdbool.h>
+#include <pthread.h>
+
+struct thread_data{
+    pthread_mutex_t *mutex;
+    int wait_to_obtain_ms;
+    int wait_to_release_ms;
+    bool thread_complete_success;
+};
+
+bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex, int wait_to_obtain_ms, int wait_to_release_ms);
+
+#endif
