0f354479a9c5f6e108ae94095880b67372cafc9b
samal <samal@example.com>
2026-08-19 16:38:41 +0500

    Add correct Buildroot package configuration for assignment 4

---
+++ buildroot/package/aesd-assignments/aesd-assignments.mk
@@ -0,0 +1,18 @@
+AESD_ASSIGNMENTS_VERSION = 'master'
+AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-87011663094s-del.git'
+AESD_ASSIGNMENTS_SITE_METHOD = git
+
+define AESD_ASSIGNMENTS_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+endef
+
+define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+	$(INSTALL) -d -m 0755 $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/finder-app/conf
+	$(INSTALL) -m 0644 $(@D)/finder-app/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+endef
+
+$(eval $(generic-package))
